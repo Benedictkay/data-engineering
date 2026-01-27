eb8c53948ded4ab9b5175435fc3e3360f883f6cd
KALELI BENEDICT <108403160+Benedictkay@users.noreply.github.com>
2026-01-27 11:15:41 +0000

    Update dockerfile

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,99 +1,69 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+import argparse
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
-
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-
-def ingest_data(
-        url: str,
-        engine,
-        target_table: str,
-        chunksize: int = 100000,
-) -> pd.DataFrame:
+def main():
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+    parser.add_argument('--pg-user', required=True, help='Postgres username')
+    parser.add_argument('--pg-pass', required=True, help='Postgres password')
+    parser.add_argument('--pg-host', required=True, help='Postgres host')
+    parser.add_argument('--pg-port', default='5432', help='Postgres port')
+    parser.add_argument('--pg-db', required=True, help='Postgres database name')
+    parser.add_argument('--target-table', required=True, help='Target table name')
+    parser.add_argument('--url', default='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz', help='URL of the CSV file')
+    parser.add_argument('--chunksize', type=int, default=100000, help='Chunk size for iteration')
+    
+    args = parser.parse_args()
+    
+    # Data types
+    dtype = {
+        "VendorID": "Int64",
+        "passenger_count": "Int64",
+        "trip_distance": "float64",
+        "RatecodeID": "Int64",
+        "store_and_fwd_flag": "string",
+        "PULocationID": "Int64",
+        "DOLocationID": "Int64",
+        "payment_type": "Int64",
+        "fare_amount": "float64",
+        "extra": "float64",
+        "mta_tax": "float64",
+        "tip_amount": "float64",
+        "tolls_amount": "float64",
+        "improvement_surcharge": "float64",
+        "total_amount": "float64",
+        "congestion_surcharge": "float64"
+    }
+    
+    parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
+    
+    # Create engine
+    engine = create_engine(f'postgresql://{args.pg_user}:{args.pg_pass}@{args.pg_host}:{args.pg_port}/{args.pg_db}')
+    
+    # Create iterator
     df_iter = pd.read_csv(
-        url,
+        args.url,
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=chunksize
+        chunksize=args.chunksize
     )
-
+    
+    # Get first chunk and create table
     first_chunk = next(df_iter)
-
-    first_chunk.head(0).to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="replace"
-    )
-
-    print(f"Table {target_table} created")
-
-    first_chunk.to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="append"
-    )
-
-    print(f"Inserted first chunk: {len(first_chunk)}")
-
+    first_chunk.head(n=0).to_sql(name=args.target_table, con=engine, if_exists='replace')
+    print(f"Table {args.target_table} created")
+    
+    # Insert first chunk
+    first_chunk.to_sql(name=args.target_table, con=engine, if_exists='append')
+    print(f"Inserted first chunk: {len(first_chunk)} rows")
+    
+    # Insert remaining chunks
     for df_chunk in tqdm(df_iter):
-        df_chunk.to_sql(
-            name=target_table,
-            con=engine,
-            if_exists="append"
-        )
-        print(f"Inserted chunk: {len(df_chunk)}")
-
-    print(f'done ingesting to {target_table}')
-
-def main():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    year = 2021
-    month = 1
-    chunksize = 100000
-    target_table = 'yellow_taxi_data'
-
-    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-    url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-
-    url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
-
-    ingest_data(
-        url=url,
-        engine=engine,
-        target_table=target_table,
-        chunksize=chunksize
-    )
+        df_chunk.to_sql(name=args.target_table, con=engine, if_exists='append')
+    
+    print("Data ingestion complete!")
 
 if __name__ == '__main__':
     main()
